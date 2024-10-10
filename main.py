a75200790526626885a77375c74083c1f463aeb0
tomin <ftouming@gmail.com>
2024-10-10 19:14:43 +0800

    fix  main.py bug

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from fmc4030 import FMC4030
 
 fmc4030 = FMC4030()
 fmc4030.open_device()
-pos = fmc4030.get_axis_current_pos()
+pos = fmc4030.get_axis_current_pos(0)
 print(pos)
 fmc4030.close_device()
