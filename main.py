55df13d6a9a215f66170d3c656c200174366e154
tomin <ftouming@gmail.com>
2024-10-12 19:49:11 +0800

    fix main code

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 
-from fmc4030 import FMC4030
+from fmc4030 import FMC4030,MMWBraket
 
 
 
 def main():
     fmc4030 = FMC4030()
-    fmc4030.open_device()
-    pos = fmc4030.get_axis_current_pos(0)
-    fmc4030.jog_single_axis_relative()
-    print(pos)
-    fmc4030.close_device()
+    mmwb = MMWBraket(fmc4030)
+    mmwb.home_axis()
+    mmwb.jog_x(100)
+    mmwb.jog_y(200)
 
 if __name__ =="__main__":
     main()
