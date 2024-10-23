b9edee9b3d5de0badac076532b1261be63d16532
tomin <tomin@example.com>
2024-10-23 20:27:13 +0800

    add mmwave

--- mmwave/mmwave.py
+++ mmwave/mmwave.py
@@ -5,10 +5,10 @@ class mmwave():
     def __init__(self,):
         pass
 
-    def run(self,):
-        pass
+    def run(self,datadir:str,record_time:int):
+        cmd = f"mmwave -d {datadir} --configure --record --time {record_time}"
+        return subprocess.run(cmd)
 
-    def get(self,):
-        pass
-
-    
+    def get(self,datadir:str,savedir:str):
+        cmd = f"scp -r -O root@192.168.33.180:{datadir} {savedir}"
+        return subprocess.run(cmd)
